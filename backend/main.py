7a568dc61185a45cc0102d479aa330d14e4c981e
itchybunion6235 <adamko.zilla@gmail.com>
2020-03-05 12:38:47 +0100

    "backend testing"

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, make_response, request
+from flask import Flask, make_response, request, jsonify
 import json
 
 app = Flask(__name__)
@@ -6,3 +6,16 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/login', methods=['POST'])
+def login():
+        if not request.is_json:
+            return jsonify({'msg': 'Wrong format'}), 400
+
+        username = request.json.get('username',None)
+        password = request.json.get('password', None)
+        if not username:
+            return jsonify({'msg': 'Missing username'}) , 400
+        if not password:
+            return jsonify({'msg': 'Missing password'}) , 400
+        return jsonify({'msg': 'Login sucessful'}) , 200
